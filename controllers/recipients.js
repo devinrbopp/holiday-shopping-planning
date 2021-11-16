33ae4dd447179907eafec0cd55a179fe7a853fd5
devinrbopp <devinrbopp@gmail.com>
2021-11-15 19:46:12 -0700

    interest selection framework created

--- controllers/recipients.js
+++ controllers/recipients.js
@@ -23,12 +23,25 @@ router.get('/new', isLoggedIn, (req,res) => {
 
 // POST create new recipient form
 router.post('/', isLoggedIn, (req, res) => {
-    console.log(req.body)
+    console.log('form content!!!!!', req.body)
+    let interests = []
+    console.log('interests defined')
+    if (req.body.interest1 === 'on') {
+        interests.push('interest 1')
+    }
+    console.log('checked interest 1')
+    if (req.body.interest2 === 'on') {
+        interests.push('interest 2')
+    }
+    console.log('checked interest 2')
+    let interestsStr = interests.join(',')
+    console.log('joined interests')
     db.recipient.create({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         relationship: req.body.relationship,
-        userId: req.body.userId
+        userId: req.body.userId,
+        interests: interestsStr
     })
     .then((recipient) => {
         res.redirect('/recipients')
