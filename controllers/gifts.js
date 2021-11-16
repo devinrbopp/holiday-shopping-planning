435744397d2ef9208826746708d60eb98ad2a970
devinrbopp <devinrbopp@gmail.com>
2021-11-16 14:01:18 -0700

    can add etsy items to gifts table

--- controllers/gifts.js
+++ controllers/gifts.js
@@ -17,6 +17,34 @@ router.get('/results', (req, res) => {
     axios.get(`https://openapi.etsy.com/v3/application/listings/active?client_id=${process.env.ETSY_API_KEY}&keywords=${keyword}`)
     .then(apiResults => {
         console.log(apiResults.data.results)
+        res.render('gifts/results', {results: apiResults.data.results})
+    })
+    .catch(error => {
+        console.error
+    })
+})
+
+// POST add a gift
+router.post('/', (req, res) => {
+    db.gift.create({
+        name: req.body.name,
+        store: req.body.store
+    })
+    .then( gift => {
+        res.redirect('/gifts')
+    })
+    .catch( error => {
+        console.error
+    })
+})
+
+// show a etsy product, and have the option to add it to a recipient
+router.get('/:etsyId', (req, res) => {
+    let etsyId = req.params.etsyId
+    // axios query
+    axios.get(`https://openapi.etsy.com/v3/application/listings/${etsyId}?client_id=${process.env.ETSY_API_KEY}&includes=images`)
+    .then(apiResults => {
+        res.render('gifts/new', {result: apiResults.data})
     })
     .catch(error => {
         console.error
