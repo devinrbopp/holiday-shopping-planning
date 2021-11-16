ca38540884f51a156aeaa8ac50f71f5729874838
devinrbopp <devinrbopp@gmail.com>
2021-11-16 07:52:37 -0700

    added modals to recipient page

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('it is working!!!!!!!!!!!')
-    // materialize css boilerplate
-    var elems = document.querySelectorAll('select')
-    var instances = M.FormSelect.init(elems, {})
-  })
+M.AutoInit();
